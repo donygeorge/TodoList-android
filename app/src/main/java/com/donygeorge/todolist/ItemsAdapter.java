a893574739b3adb9fec008f1d231dfde24e61bce
Dony George <donygeorge@gmail.com>
2017-08-23 07:34:15 -0700

    Add icons for priority + fix date format

--- app/src/main/java/com/donygeorge/todolist/ItemsAdapter.java
+++ app/src/main/java/com/donygeorge/todolist/ItemsAdapter.java
@@ -2,11 +2,14 @@ package com.donygeorge.todolist;
 
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import java.text.SimpleDateFormat;
 
 import java.util.ArrayList;
 
@@ -25,8 +28,25 @@ public class ItemsAdapter extends ArrayAdapter<Item> {
         }
         final TextView itemTextView = (TextView) convertView.findViewById(R.id.itemTextView);
         itemTextView.setText(item.text);
+        if (item.completed) {
+            itemTextView.setPaintFlags(itemTextView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
         final TextView dateTextView = (TextView) convertView.findViewById(R.id.dateTextView);
-        dateTextView.setText(item.completionDate.toString());
+        SimpleDateFormat format = new SimpleDateFormat("EEE, MMM dd yyyy");
+        dateTextView.setText(format.format(item.completionDate));
+        final ImageView priorityImageView = (ImageView) convertView.findViewById(R.id.priorityImageView);
+        switch (item.priority) {
+            case HIGH:
+                priorityImageView.setImageResource(R.drawable.circle_red);
+                break;
+            case MEDIUM:
+                priorityImageView.setImageResource(R.drawable.circle_orange);
+                break;
+            case LOW:
+                priorityImageView.setImageResource(R.drawable.circle_yellow);
+                break;
+        }
+
         return convertView;
     }
 }
